81c37c0e3ac3f1d8cea2d41d9f1cdf12687ee6c8
kagaev <kyamranagaev@gmail.com>
2020-07-16 13:07:07 +0300

    Initial Commit

---
+++ Homework#1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework#1
+//
+//  Created by Кямран Агаев on 16.07.2020.
+//  Copyright © 2020 Кямран Агаев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
